ab5d57d08fd5944bb0bf73e2ab5b930c1bc670d2
yuvi-mittal <mittalyuvi02@gmail.com>
2025-06-30 22:38:24 +0530

    revised code

--- lec/main.cpp
+++ lec/main.cpp
@@ -13,7 +13,6 @@
 // 	cout<<( 17>>1 )<<endl;  // right shift 
 // 	cout<<( a^b )<<endl;
 
-
 // 	return 0;
 
 // }
@@ -25,40 +24,22 @@ using namespace std;
 
 int main() {
 
-	int n;
-	cin>> n;
-
-	int ans =0;
-
-	while(n != 0){
-
-		int bit = n & 1;
-
-		ans = (10 * ans) + bit;
-
-		n = n>>1;
-	}
-
-	cout<<ans<<endl;
-	return 0;
-
-    // int n;
-    // cin >> n;
+    int n;
+    cin >> n;
 
 
-    // int ans  = 0;
-    // int i = 0;
-    // while(n != 0 ) {
+    int ans  = 0;
+    int i = 0;
+    while(n != 0 ) {
 
-    //     int bit  = n & 1;
+        int bit  = n & 1;
 
-    //     ans = (bit * pow(10, i) ) + ans;
+        ans = (bit * pow(10, i) ) + ans;
 
-    //     n = n >> 1;
-    //     i++;
+        n = n >> 1;
+        i++;
 
-    // }
-
-    // cout<<" Answer is " << ans << endl;
-    // return 0;
+    }
+    cout<< ans << endl;
+    return 0;
 }
