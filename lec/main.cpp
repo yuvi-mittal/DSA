757d9fe6ba61d7901b9dde6b85ef4f6265c46e83
yuvi-mittal <mittalyuvi02@gmail.com>
2025-06-30 20:45:48 +0530

    lecture on bitwise

---
+++ lec/main.cpp
@@ -0,0 +1,64 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+// 	int a =3;
+// 	int b = 2;
+
+// 	cout<<( a&b )<<endl;
+// 	cout<<( ~a )<<endl;
+// 	cout<<( a | b )<<endl;
+// 	cout<<( a^b )<<endl;
+// 	cout<<( 17>>1 )<<endl;  // right shift 
+// 	cout<<( a^b )<<endl;
+
+
+// 	return 0;
+
+// }
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+
+int main() {
+
+	int n;
+	cin>> n;
+
+	int ans =0;
+
+	while(n != 0){
+
+		int bit = n & 1;
+
+		ans = (10 * ans) + bit;
+
+		n = n>>1;
+	}
+
+	cout<<ans<<endl;
+	return 0;
+
+    // int n;
+    // cin >> n;
+
+
+    // int ans  = 0;
+    // int i = 0;
+    // while(n != 0 ) {
+
+    //     int bit  = n & 1;
+
+    //     ans = (bit * pow(10, i) ) + ans;
+
+    //     n = n >> 1;
+    //     i++;
+
+    // }
+
+    // cout<<" Answer is " << ans << endl;
+    // return 0;
+}
