3eb2a15a671ffcf5e342ee18854b7a2ae9775599
Yuvi <128018763+yuvi-mittal@users.noreply.github.com>
2025-07-05 23:22:23 +0530

    Update twoSum.cpp

--- arrays/twoSum.cpp
+++ arrays/twoSum.cpp
@@ -17,13 +17,3 @@ class Solution {
 		}
 	};
 
-class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
-        int ans = 0;
-        for(int i = 0; i < nums.size(); i++) {
-            ans = ans ^ nums[i];
-        }
-        return ans;
-    }
-};
