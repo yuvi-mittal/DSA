13cd4fe05ee093bf5526bdd7d3b4b30dd62b06eb
Yuvi <128018763+yuvi-mittal@users.noreply.github.com>
2025-07-20 14:00:38 +0530

    Update single sum1.cpp

--- arrays/single sum1.cpp
+++ arrays/single sum1.cpp
@@ -5,6 +5,7 @@ public:
         for (int num : nums) {
             freq[num]++;
         }
+        
         for (auto& pair : freq) {
             if (pair.second == 1)
                 return pair.first;
